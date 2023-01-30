e9e3c53f3a2537ccbb03ee2a9d27e3ad3bdf66cd
Manuel Atance <atance3@hotmail.com>
2023-01-30 20:45:32 +0100

    Grid created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,55 @@
 const Header = () => {
 	return (
-		<div className="headerContainer">
-			<h1>Hi, I'm Manuel</h1>
-		</div>
+		<>
+			<div className="container">
+				<div className="wrapper outsider">
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+			<div className="container">
+				<div className="wrapper">
+					<h1 className="name">Ryuusei3</h1>
+					<div></div>
+				</div>
+			</div>
+			<div className="container">
+				<div className="wrapper hero">
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+			<div className="container">
+				<div className="wrapper inverse hero">
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+			<div className="container">
+				<div className="wrapper tricol hero">
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+			<div className="container">
+				<div className="wrapper full hero">
+					<div></div>
+				</div>
+			</div>
+			<div className="container">
+				<div className="wrapper hero">
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+			<div className="container">
+				<div className="wrapper outsider">
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+		</>
 	)
 }
 
